37640f6ae67af5adeb191dbc7f685af14763abac
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-18 20:53:26 +0900

    [정재원] 프론트 합치기

--- src/main/java/site/metacoding/everytimeclone/web/PostController.java
+++ src/main/java/site/metacoding/everytimeclone/web/PostController.java
@@ -1,5 +1,18 @@
 package site.metacoding.everytimeclone.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
 public class PostController {
+
+    // 메인
+    @GetMapping("/")
+    public String main() {
+        return "/post/main";
+    }
 
 }
