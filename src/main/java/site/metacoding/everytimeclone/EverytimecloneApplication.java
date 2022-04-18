edb042281f5014c4d7837ae86ff8fbb25a53737f
jaewon2336 <xldzjqpf1588@naver.com>
2022-04-18 19:56:05 +0900

    [정재원] initial commit

---
+++ src/main/java/site/metacoding/everytimeclone/EverytimecloneApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.everytimeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverytimecloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverytimecloneApplication.class, args);
+	}
+
+}
